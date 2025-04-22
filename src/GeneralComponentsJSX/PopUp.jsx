9d6b1f721fb91edb061b32076b3d42bc8e39f5fb
Alma Yovell <alma.yovell@gmail.com>
2025-04-23 00:33:51 +0300

    starting to  add diagram

---
+++ src/GeneralComponentsJSX/PopUp.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../GeneralComponentsCss/PopUp.css';
+
+const PopUp = ({ isVisible, onClose, title, content }) => {
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <div className="popup-overlay">
+      <div className="popup-content">
+        <button className="popup-close-button" onClick={onClose}>
+          {/* &times; */}
+          <img src={process.env.PUBLIC_URL + '/XBtn.png'} alt="Close" className="XBtn" />
+        </button>
+        <h2 className="popup-title" >{title}</h2> {/* Set the color dynamically */}
+        <div className="popup-body">
+          <div className="popup-body-text">
+            {content}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopUp;
